3349dbc9899364e07261e51511633c65d82cc60e
Wulan <ASUS@LAPTOP-781R3HVH>
2022-11-21 15:37:09 +0700

    add class CircularList

--- Exercise3/Program.cs
+++ Exercise3/Program.cs
@@ -8,10 +8,19 @@ namespace Exercise3
 {
     class node
     {
-        /**/
+        /*creates Nodes for the circular nexted list*/
         public int rollNumber;
         public string name;
         public node next;
+    }
+    class CircularList
+    {
+        node LAST;
+
+        public CircularList()
+        {
+            LAST = null;
+        }
     }
     internal class Program
     {
