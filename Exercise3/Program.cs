377b6cdcc2d98c7b10551b22b475ad6bfb592a9c
Wulan <ASUS@LAPTOP-781R3HVH>
2022-11-21 16:20:03 +0700

    add a node in the list

--- Exercise3/Program.cs
+++ Exercise3/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Exercise3
 {
@@ -20,6 +22,47 @@ namespace Exercise3
         public CircularList()
         {
             LAST = null;
+        }
+
+        public void addNote() // add a node in the list 
+        {
+            int rollNo;
+            string nm;
+            Console.Write("\nEnter the roll number of the student: ");
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the roll name of the student: ");
+            nm = Console.ReadLine();
+            node newnode = new node();
+            newnode.rollNumber = rollNo;
+            newnode.name = nm;
+            //if the node to be inserted is the first node
+            if ((LAST == null) || (rollNo == LAST.rollNumber))
+            {
+                if ((LAST != null) && (rollNo == LAST.rollNumber))
+                {
+                    Console.WriteLine();
+                    return;
+                }
+                newnode.next = LAST;
+                LAST = newnode;
+                return;
+            }
+            node previous, current;
+            previous = LAST;
+            current = LAST;
+
+            while ((current != null) && (rollNo == current.rollNumber))
+            {
+                if (rollNo == current.rollNumber)
+                {
+                    Console.WriteLine();
+                    return;
+                }
+                previous.next = current;
+                previous.next = newnode;
+            }
+            newnode.next = current;
+            previous.next = newnode;
         }
 
         public bool Search(int rollNo, ref node previous, ref node current)
