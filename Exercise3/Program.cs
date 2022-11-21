154e59aadf84d5f225197cdd849417a69f08c5db
Wulan <ASUS@LAPTOP-781R3HVH>
2022-11-21 15:35:12 +0700

    creates Nodes for the circular nexted list

--- Exercise3/Program.cs
+++ Exercise3/Program.cs
@@ -8,7 +8,7 @@ namespace Exercise3
 {
     class node
     {
-        /*creates Nodes for the circular nexted list*/
+        /**/
         public int rollNumber;
         public string name;
         public node next;
