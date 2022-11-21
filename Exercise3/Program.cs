0b994437cd9f97d7786b84cda445691eb3bef814
Wulan <ASUS@LAPTOP-781R3HVH>
2022-11-21 15:50:47 +0700

    Traverses all the nodes all the nodes of the list

--- Exercise3/Program.cs
+++ Exercise3/Program.cs
@@ -34,7 +34,31 @@ namespace Exercise3
             else
                 return (false); 
         }
-           
+        public bool listEmpty()
+        {
+            if (LAST == null)
+                return true;
+            else
+                return false;
+        }
+
+        public void traverse()
+        {
+            if (listEmpty())
+                Console.WriteLine("\nList is empty");
+            else
+            {
+                Console.WriteLine("\nRecords in the list are:\n");
+                node currentNode;
+                currentNode = LAST.next;
+                while (currentNode != LAST)
+                {
+                    Console.Write(currentNode.rollNumber + "   " + currentNode.name + "\n");
+                    currentNode = currentNode.next;
+                }
+                Console.Write(LAST.rollNumber + "    " + LAST.name + "\n");
+            }
+        }
     }
     internal class Program
     {
